8baac53158b74388bee085127829d5da894ea3a8
Oscar Mira <valldrac@molly.im>
2024-07-13 12:06:37 +0200

    Revert "Remove more ProxyInfo references."
    This reverts commit db6767dd0c8ed11997ff9e7f57594f5818f60684.

--- p2p/base/basic_packet_socket_factory.h
+++ p2p/base/basic_packet_socket_factory.h
@@ -40,6 +40,8 @@ class RTC_EXPORT BasicPacketSocketFactory : public PacketSocketFactory {
   AsyncPacketSocket* CreateClientTcpSocket(
       const SocketAddress& local_address,
       const SocketAddress& remote_address,
+      const ProxyInfo& proxy_info,
+      const std::string& user_agent,
       const PacketSocketTcpOptions& tcp_options) override;
 
   std::unique_ptr<AsyncDnsResolverInterface> CreateAsyncDnsResolver() override;
