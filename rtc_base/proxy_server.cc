032a13de1ccec94a232dd390a2e90e39adb6bd56
Oscar Mira <valldrac@molly.im>
2024-08-19 17:48:45 +0200

    Revert "Remove SOCKS5 support"
    This reverts commit b5f2b17fd8a44b9a4e6b47f10b405f070814041a.

--- rtc_base/proxy_server.cc
+++ rtc_base/proxy_server.cc
@@ -149,4 +149,8 @@ void ProxyBinding::Destroy() {
   SignalDestroyed(this);
 }
 
+AsyncProxyServerSocket* SocksProxyServer::WrapSocket(Socket* socket) {
+  return new AsyncSocksProxyServerSocket(socket);
+}
+
 }  // namespace rtc
